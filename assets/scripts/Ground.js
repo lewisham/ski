9b0f9550cc8cbb909c6aa8812ead3918d2bd748a
张功建 <zhanggongjian@weile.com>
2020-08-15 15:56:49 +0800

    test push

--- assets/scripts/Ground.js
+++ assets/scripts/Ground.js
@@ -2,7 +2,7 @@ cc.Class({
     extends: cc.Component,
 
     start () {
-        var list = cc.BezierUtils.CalcCurvePoint([cc.v2(10, 400), cc.v2(400, 100), cc.v2(1000, 300)]);
+        var list = cc.BezierUtils.CalcCurvePoint([cc.v2(10, 400), cc.v2(400, 100), cc.v2(1100, 300)]);
         this._points = cc.BezierUtils.CalcPoints(list);
         var chain = this.getComponent(cc.PhysicsChainCollider);
         chain.points = this._points;
